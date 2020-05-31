0ba64c8d0851037388b429d38f5ab39de2208aa1
Chentao Zhou <zctlyr@gmail.com>
2020-05-31 15:36:23 +0100

    ReConstructBinaryTree

---
+++ src/tree/ReConstructBinaryTree.java
@@ -0,0 +1,35 @@
+package tree;
+
+import java.util.Arrays;
+
+public class ReConstructBinaryTree {
+	 
+	  public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+	  public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+		  if(pre.length == 0) {
+			  return null;
+		  }
+		  int rootval = pre[0];
+		  TreeNode root = new TreeNode(rootval);
+		  if(pre.length == 1) {
+			  return root;
+		  }
+		  int rootIndex = 0;
+		  for(int i=0;i<in.length;i++) {
+			  if(rootval == in[i]) {
+				  rootIndex = i;
+				  break;
+			  }
+		  }
+		  root.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, rootIndex+1),Arrays.copyOfRange(in, 0, rootIndex));
+		  root.right = reConstructBinaryTree(Arrays.copyOfRange(pre, rootIndex+1, pre.length),Arrays.copyOfRange(in, rootIndex+1, in.length));
+		  
+		  return root;
+	  }
+	 
+}
