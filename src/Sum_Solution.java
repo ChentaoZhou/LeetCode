1fd4e9cd04e79ad481562a0f6eb6693c60d068d4
Chentao Zhou <zctlyr@gmail.com>
2020-06-07 16:12:22 +0100

    6.7

---
+++ src/Sum_Solution.java
@@ -0,0 +1,9 @@
+
+public class Sum_Solution {
+	public int Sum_Solution(int n) {
+	        int sum = n;
+	        boolean ans = (n>0)&&((sum+=Sum_Solution(n-1))>0);
+	        return sum;
+	        
+	    }
+}
