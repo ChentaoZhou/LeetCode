1949edd725b6f33b856b06fdfb54190e78079b4f
Chentao Zhou <zctlyr@gmail.com>
2020-06-09 14:38:25 +0100

    6.8

---
+++ src/tree/ConverList.java
@@ -0,0 +1,36 @@
+package tree;
+
+import java.util.ArrayList;
+
+public class ConverList{
+
+public TreeNode Convert(TreeNode pRootOfTree) {
+        if(pRootOfTree == null){
+            return null;
+        }
+        ArrayList<TreeNode> list = new ArrayList<>();
+        Convert(pRootOfTree, list);
+        return Convert(list);
+
+    }
+    //中序遍历，在list中按遍历顺序保存
+    public void Convert(TreeNode pRootOfTree, ArrayList<TreeNode> list){
+        if(pRootOfTree.left != null){
+            Convert(pRootOfTree.left, list);
+        }
+
+        list.add(pRootOfTree);
+
+        if(pRootOfTree.right != null){
+            Convert(pRootOfTree.right, list);
+        }
+    }
+    //遍历list，修改指针
+    public TreeNode Convert(ArrayList<TreeNode> list){
+        for(int i = 0; i < list.size() - 1; i++){
+            list.get(i).right = list.get(i + 1);
+            list.get(i + 1).left = list.get(i);
+        }
+        return list.get(0);
+    }
+}
--- src/tree/PrintZ.java
+++ src/tree/PrintZ.java
@@ -1,7 +1,38 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class PrintZ {
 	public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+		if(pRoot==null) return res;
+		Stack<TreeNode> old = new Stack<TreeNode>();
+		Stack<TreeNode> curr = null;
+		old.push(pRoot);
+		int layer = 1;
+		
+		while(!old.isEmpty() || !curr.isEmpty() ) {
+			
+			while(res.size()<layer) {
+				res.add(new ArrayList<Integer>());
+			}
+			
+			curr = new Stack<TreeNode>();
+			while(!old.isEmpty()) {
+				TreeNode temp = old.pop();
+				res.get(layer-1).add(temp.val);
+				//奇数层，先加左子再右子
+				if(temp.left!=null && layer%2 != 0)  curr.add(temp.left);
+				if(temp.right!=null && layer%2 != 0) curr.add(temp.right);
+				//偶数层，先加右子再左子
+				if(temp.right!=null && layer%2 == 0) curr.add(temp.right);
+				if(temp.left != null && layer%2 == 0) curr.add(temp.left);
+			}
+			layer++;
+			old = curr;
+		}
+		return res;
 		
 	}
 
