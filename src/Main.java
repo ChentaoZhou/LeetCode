4a11f23ef5a9ce223387ca56758652193afb0519
Chentao Zhou <zctlyr@gmail.com>
2020-07-19 01:19:39 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,74 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
+import linkedList.ListNode;
+
 public class Main {
-	 static boolean flag = false;
-	    public static boolean hasPath(char[] matrix, int rows, int cols, char[] str)
-	    {
-	        
-	        int[][] map = new int[rows][cols];
-	        for(int index=0;index<matrix.length;index++){
-	            if(matrix[index]==str[0]){
-	            	System.out.println("row: "+index/cols+" col: "+index%cols+" P: "+matrix[index]);
-	                find(matrix,map,index/cols,index%cols,new String(str));
+	private static class ListNode {
+	    int val;
+	    ListNode next = null;
+
+	    ListNode(int val) {
+	        this.val = val;
+	    }
+	}
+	 public static ListNode Merge(ListNode list1,ListNode list2) {
+	        if(list1==null) return list2;
+	        if(list2==null) return list1;
+	        ListNode vhead = null,cur = null;
+	        while(list1!=null && list2!=null){
+	            if(list1.val<=list2.val){
+	            	
+	            	if(vhead==null) {
+	            		vhead=list1;
+	            		cur= list1;
+	            	}else {
+		                cur.next = list1;
+		                cur = cur.next;
+	            	}
+	            	list1 = list1.next;
+	            }
+	            else if(list1.val>list2.val) {
+	            	
+	            	if(vhead==null) {
+	            		vhead=list2;
+	            		cur = list2;
+	            	}
+	            
+	            else{
+	                cur.next = list2;
+	                cur=cur.next;
+	                
+	            }
+	            	list2 = list2.next;
 	            }
 	        }
-	        return flag;
+	        if(list1!=null) cur.next = list1;
+	        if(list2!=null) cur.next = list2;
+	        System.out.println(vhead.val);
+	        return vhead;
+	        
 	    }
-	    public static void find(char[] matrix,int[][] map,int row, int col,String str){
-	       
-	        if(row<0||col<0||row>map.length-1||col>map[0].length-1||map[row][col]==-1) return;
-	         int index = row*map[0].length+col;
-	        if(str.length()==1 && matrix[index]==str.charAt(0)) {
-	            flag = true;
-	            return;
-	        }
-	        if(matrix[index] == str.charAt(0)){
-	            str = str.substring(1);
-	            map[row][col]=-1;
-	            find(matrix,map,row+1,col,str);
-	            find(matrix,map,row-1,col,str);
-	            find(matrix,map,row,col+1,str);
-	            find(matrix,map,row,col-1,str);
-	            
-	        }
-	        map[row][col] = 0;
-	    }
-	   public static void main(String[] args) {
-		   char[] m = {'A','B','C','E','S','F','C','S','A','D','E','E'};
-		   char[] n = {'S','E','E'};
-		   
-		   System.out.println(hasPath(m,3,4,n));
-	   }
+	 public static void main(String[] args) {
+		 ListNode nod1 = new ListNode(1);
+		 ListNode nod2 = new ListNode(3);
+		 ListNode nod3 = new ListNode(5);
+		 ListNode nod4 = new ListNode(8);
+		 ListNode nod5 = new ListNode(13);
+		 nod1.next = nod2;nod2.next = nod3;//nod3.next=nod4;nod4.next = nod5;
+		 ListNode node1 = new ListNode(2);
+		 ListNode node2 = new ListNode(4);
+		 ListNode node3 = new ListNode(6);
+		 ListNode node4 = new ListNode(10);
+		 ListNode node5 = new ListNode(12);
+		 node1.next=node2; node2.next = node3; //node3.next = node4; node4.next = node5;
+		 ListNode result = Merge(nod1, node1);
+		 while(result!=null) {
+			 System.out.print(result.val+"--> ");
+			 result = result.next;
+		 }
+		 
+	 }
+	
 }
+
